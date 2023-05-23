368f6d184bc951bc6a106cdcb2fe7cbe32b8980f
SD-IITKGP <jaihindsourabh@kgpian.iitkgp.ac.in>
2023-05-23 18:53:21 +0530

    minor update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const { HOW3Protocol } = require("./lib/index.js");
+const { HOW3Protocol } = require("how3-dip");
 async function check() {
-    console.log(await HOW3Protocol.isAvailable());
+    console.log(await HOW3Protocol.isAvailable("vitalik.apt"));
 }
 async function check2() {
     console.log(await HOW3Protocol.register());
 }
-check2();
+check();
